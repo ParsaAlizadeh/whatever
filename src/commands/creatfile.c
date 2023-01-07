ca86a0fce6b62c52a62fed5da771aa5706fe41cf
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-01-07 16:29:17 +0330

    Finish creatfile

--- src/commands/creatfile.c
+++ src/commands/creatfile.c
@@ -2,15 +2,17 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "string.h"
+#include <string.h>
+#include <errno.h>
+#include "../fileutil.h"
 
 typedef struct {
-    char *file;
+    char *path;
 } creatfile_t;
 
 static void *make(void) {
     creatfile_t *this = malloc(sizeof(creatfile));
-    this->file = NULL;
+    this->path = NULL;
     return this;
 }
 
@@ -18,7 +20,7 @@ static int set_opt(void *_this, int c, char *argv) {
     creatfile_t *this = _this;
     switch (c) {
     case 'f':
-        this->file = argv;
+        this->path = argv;
         break;
     default:
         fprintf(stderr, "creatfile: unexpected flag %c\n", c);
@@ -29,11 +31,29 @@ static int set_opt(void *_this, int c, char *argv) {
 
 static void run(void *_this) {
     creatfile_t *this = _this;
-    if (this->file == NULL) {
+    if (this->path == NULL) {
         fprintf(stderr, "creatfile: option \"-f\" is required\n");
         return;
     }
-    fprintf(stderr, "dry-run creatfile file=[[%s]]\n", this->file);
+    char *dir = fu_dirname(this->path);
+    if (mkdir_p(dir) == -1) {
+        fprintf(stderr, "creatfile: failed to create directory: %s\n",
+            strerror(errno));
+        return;
+    }
+    free(dir);
+    if (fu_exists(this->path)) {
+        fprintf(stderr, "creatfile: file \"%s\" exists\n", this->path);
+        return;
+    }
+    FILE *file;
+    if ((file = fopen(this->path, "w")) == NULL) {
+        fprintf(stderr, "creatfile: failed to create file \"%s\": %s\n",
+            this->path, strerror(errno));
+        return;
+    }
+    fclose(file);
+    fprintf(stderr, "creatfile: done\n");
 }
 
 const command creatfile = {
