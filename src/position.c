936e2926270f4caefb9fff1dc7b193bcd7c761a8
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-01-29 18:46:43 +0330

    Modify selection range rule

--- src/position.c
+++ src/position.c
@@ -2,16 +2,24 @@
 
 #include <stdlib.h>
 
-int pos_lesseq(pos_t a, pos_t b) {
+int pos_eq(pos_t a, pos_t b) {
+    return a.line == b.line && a.col == b.col;
+}
+
+int pos_less(pos_t a, pos_t b) {
     if (a.line != b.line)
         return a.line < b.line;
-    return a.col <= b.col;
+    return a.col < b.col;
+}
+
+int pos_lesseq(pos_t a, pos_t b) {
+    return pos_less(a, b) || pos_eq(a, b);
 }
 
 int pos_inrange(pos_t afrom, pos_t apos, pos_t ato) {
     if (afrom.line < 0 || ato.line < 0)
         return 0;
-    if (pos_lesseq(afrom, apos) && pos_lesseq(apos, ato))
+    if (pos_lesseq(afrom, apos) && pos_less(apos, ato))
         return 1;
-    return pos_lesseq(ato, apos) && pos_lesseq(apos, afrom);
+    return pos_lesseq(ato, apos) && pos_less(apos, afrom);
 }
--- src/position.h
+++ src/position.h
@@ -5,7 +5,13 @@ typedef struct {
     int line, col;
 } pos_t;
 
-int pos_lesseq(pos_t a, pos_t b);
+int pos_eq(pos_t, pos_t);
+int pos_less(pos_t, pos_t);
+int pos_lesseq(pos_t, pos_t);
+
+/*
+ * checks for afrom <= apos < ato or ato <= apos < aform
+ */
 int pos_inrange(pos_t afrom, pos_t apos, pos_t ato);
 
 #endif
