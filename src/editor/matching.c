cf835889bec917d1c2b0a0447b1a968a83f94c36
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-01-31 10:02:10 +0330

    Find matching pairs faster

--- src/editor/matching.c
+++ src/editor/matching.c
@@ -5,77 +5,89 @@
 static const char matches[][3] = { "()", "[]", "{}" };
 static const int n_matches = sizeof(matches) / sizeof(matches[0]);
 
-typedef struct _list {
-    pos_t apos;
+#define N 200
+
+typedef struct {
     char chr;
-    struct _list *tail;
-} list;
+    pos_t apos;
+} item;
 
-static list *cons(pos_t apos, char chr, list *tail) {
-    list *this = malloc(sizeof(list));
-    this->apos = apos;
-    this->chr = chr;
-    this->tail = tail;
-    return this;
+static item stack[N];
+static int ind;
+
+static void init(void) {
+    ind = 0;
 }
 
-static void list_free(list *this) {
-    if (this == NULL)
+static void push(item i) {
+    if (ind >= N)
         return;
-    list_free(this->tail);
-    free(this);
+    stack[ind++] = i;
 }
 
-static list *list_erase(list *this) {
-    list *tail = this->tail;
-    free(this);
-    return tail;
+static item top(void) {
+    return stack[ind-1];
 }
 
-static pos_t _vc_match(vecline *vc, pos_t apos) {
-    pos_t now = { 0, 0 };
-    pos_t result = { -1, 0 };
-    list *stack = NULL;
-    while (result.line == -1) {
-        if (now.line >= vc_nlines(vc))
-            break;
-        line_t *line = vc_atline(vc, now.line);
-        if (now.col >= line->size) {
-            now.line++;
-            now.col = 0;
-            continue;
+static item pop(void) {
+    return stack[--ind];
+}
+
+static int movedir(vecline *vc, pos_t *apos, int dir) {
+    if (dir == 0) {
+        line_t *line = vc_atline(vc, apos->line);
+        apos->col++;
+        if (apos->col == line->size) {
+            apos->col = 0;
+            apos->line++;
         }
-        int chr = line->content[now.col];
+        if (apos->line >= vc_nlines(vc))
+            return -1;
+        return 0;
+    }
+    apos->col--;
+    if (apos->col < 0) {
+        apos->line--;
+        if (apos->line < 0)
+            return -1;
+        apos->col = vc_atline(vc, apos->line)->size - 1;
+    }
+    return 0;
+}
+
+static pos_t _vc_match(vecline *vc, pos_t apos, int dir) {
+    pos_t now = apos;
+    pos_t result = { -1, 0 };
+    init();
+    while (result.line == -1) {
+        char chr = vc_at(vc, now);
         for (int i = 0; i < n_matches; i++) {
-            if (chr == matches[i][0]) {
-                stack = cons(now, chr, stack);
+            if (chr == matches[i][dir]) {
+                push((item) { chr, now });
                 break;
             }
-            if (chr != matches[i][1])
+            if (chr != matches[i][1^dir])
                 continue;
-            if (!stack || stack->chr != matches[i][0])
+            item tp = top();
+            if (tp.chr != matches[i][dir])
                 continue;
-            if (pos_eq(now, apos))
-                result = stack->apos;
-            if (pos_eq(stack->apos, apos))
+            if (pos_eq(tp.apos, apos))
                 result = now;
-            stack = list_erase(stack);
+            pop();
         }
-        now.col++;
+        if (movedir(vc, &now, dir) == -1)
+            break;
     }
-    list_free(stack);
     return result;
 }
 
 pos_t vc_match(vecline *vc, pos_t apos) {
-    int chr = vc_at(vc, apos);
-    int matchable = 0;
-    for (int i = 0; !matchable && i < n_matches; i++)
-        for (int j = 0; !matchable && j < 2; j++)
-            matchable |= (chr == matches[i][j]);
-    if (!matchable)
-        return (pos_t) { -1, 0 };
-    return _vc_match(vc, apos);
+    char chr = vc_at(vc, apos);
+    for (int i = 0; i < n_matches; i++)
+        for (int j = 0; j < 2; j++)
+            if (chr == matches[i][j])
+                return _vc_match(vc, apos, j);
+    return (pos_t) { -1, 0 };
 }
 
 pos_t editor_curmatch(void) {
